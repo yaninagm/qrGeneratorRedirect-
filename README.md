54c6a9924a5505e12d69cf87bde396351bf9f0ab
Yanina Morales <yanigmorales@gmail.com>
2020-12-07 01:05:29 +0100

    cors fucking cors

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # qrGeneratorRedirect-
 
 spring run app.groovy 
+
+
+https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS#Preflighted_requests
--- public/hello.js
+++ public/hello.js
@@ -1,7 +1,13 @@
 $(document).ready(function() {
     $.ajax({
-        url2: "http://rest-service.guides.spring.io/greeting",
-        url:  "https://real-customer.herokuapp.com/qrByProduct/1"
+        url:  "https://real-customer.herokuapp.com/qrByProduct/1",
+        headers: { 'Access-Control-Allow-Origin': '*' },
+        headers: { 'Origin': 'http://localhost:8080/' },
+        headers: { 'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE' },
+        headers: { 'Access-Control-Allow-Headers': 'Content-Type' },
+        headers: { 'Access-Control-Request-Method': 'GET' },
+        headers: { 'Access-Control-Request-Headers': 'Content-Type' }
+
     }).then(function(data) {
         $('.greeting-id').append(data.id);
         $('.greeting-content').append(data.name);
