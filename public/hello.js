9611d6570b5d5c45f6ca46177103cb49b93c4a30
Yanina Morales <yanigmorales@gmail.com>
2020-12-08 22:39:01 +0100

    Front connected to back

--- public/hello.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $.ajax({
-        url:  "http://localhost:8080/qrByProduct?id=1"
-
-    }).then(function(data) {
-        $('.greeting-id').append(data.id);
-        $('.greeting-content').append(data.name);
-    });
-});
--- src/main/java/com/front/qrGeneratorRedirect/Infrastucture/StaticQrController.java
+++ src/main/java/com/front/qrGeneratorRedirect/Infrastucture/StaticQrController.java
@@ -2,11 +2,13 @@ package com.front.qrGeneratorRedirect.Infrastucture;
 
 import com.front.qrGeneratorRedirect.Domain.QrInfo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class StaticQrController {
 
     @Autowired
@@ -17,7 +19,10 @@ public class StaticQrController {
     }
 
     @GetMapping("/qrByProduct")
-    public QrInfo getUrlById(@RequestParam(value = "id") Long id) {
-        return staticQrService.getQrInfo(id);
+    public String getUrlById(@RequestParam(value = "id") Long id, Model model) {
+        QrInfo result = staticQrService.getQrInfo(id);
+        model.addAttribute("id",result.getId());
+        model.addAttribute("name",result.getName());
+        return "qrRedirect";
     }
 }
